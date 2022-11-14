23ead9a497f8770fb8d0c7326933bf0795d3770b
Wasif Jameel <wasif.jameel1@gmail.com>
2022-11-14 09:10:52 +0500

    analytics added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,22 @@
-import Layout from '../components/layout';
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
+
+import Layout from '../components/layout';
 
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-151666141-1');
+        `}
+      </Script>
       <Component {...pageProps} />
     </Layout>
   );

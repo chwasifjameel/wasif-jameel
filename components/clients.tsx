475706fd4a3cdd95a063fef5a83a4119c91f7b34
Wasif Jameel <wasif.jameel1@gmail.com>
2022-11-15 11:03:16 +0500

    testimonials spacing

--- components/clients.tsx
+++ components/clients.tsx
@@ -1,4 +1,5 @@
 import { CarouselProvider, Slider, Slide } from 'pure-react-carousel';
+import Image from 'next/image';
 
 const Clients = () => {
   return (
@@ -27,54 +28,56 @@ const Clients = () => {
               naturalSlideHeight={0}>
               <div className="js-flickity flex justify-center items-center">
                 <Slider>
-                  <Slide className="px-8" index={0}>
-                    <img
+                  <Slide className="px-2" index={0}>
+                    <Image
+                      width={300}
+                      height={300}
                       src="/intermedia-marketing.png"
                       alt="intermedia marketing"
                     />
                   </Slide>
-                  <Slide className=" px-8" index={1}>
-                    <img
+                  <Slide className="px-2" index={1}>
+                    <Image
                       width={300}
                       height={300}
                       src="/smart-tipz-logo.png"
                       alt="smart tipz"
                     />
                   </Slide>
-                  <Slide className=" px-8" index={2}>
-                    <img
+                  <Slide className="px-2" index={2}>
+                    <Image
                       width={300}
                       height={300}
                       src="/windswept.png"
                       alt="windswept"
                     />
                   </Slide>
-                  <Slide className=" px-8" index={3}>
-                    <img
+                  <Slide className="px-2" index={3}>
+                    <Image
                       width={300}
                       height={300}
                       src="/crowdbotics.png"
                       alt="crowdbotics"
                     />
                   </Slide>
-                  <Slide className=" px-8" index={4}>
-                    <img
+                  <Slide className="px-2" index={4}>
+                    <Image
                       width={300}
                       height={300}
                       src="/2marketing.png"
                       alt="2marketing"
                     />
                   </Slide>
-                  <Slide className=" px-8" index={5}>
-                    <img
+                  <Slide className="px-2" index={5}>
+                    <Image
                       width={300}
                       height={300}
                       src="/gulf-academy.png"
                       alt="gulf academy"
                     />
                   </Slide>
-                  <Slide className=" px-8" index={6}>
-                    <img
+                  <Slide className="px-2" index={6}>
+                    <Image
                       width={300}
                       height={300}
                       src="/happy-credit.webp"
@@ -98,54 +101,56 @@ const Clients = () => {
               naturalSlideHeight={0}>
               <div className="js-flickity flex justify-center items-center">
                 <Slider className="carousel__sliderLarge">
-                  <Slide className="carousel__inner-slideLarge" index={0}>
-                    <img
+                  <Slide className="carousel__inner-slideLarge px-5" index={0}>
+                    <Image
+                      width={300}
+                      height={300}
                       src="/intermedia-marketing.png"
                       alt="intermedia marketing"
                     />
                   </Slide>
-                  <Slide className="carousel__inner-slideLarge px-2" index={1}>
-                    <img
+                  <Slide className="carousel__inner-slideLarge px-5" index={1}>
+                    <Image
                       width={300}
                       height={300}
                       src="/smart-tipz-logo.png"
                       alt="smart tipz"
                     />
                   </Slide>
-                  <Slide className="carousel__inner-slideLarge px-2" index={2}>
-                    <img
+                  <Slide className="carousel__inner-slideLarge px-5" index={2}>
+                    <Image
                       width={300}
                       height={300}
                       src="/windswept.png"
                       alt="windswept"
                     />
                   </Slide>
-                  <Slide className="carousel__inner-slideLarge px-2" index={3}>
-                    <img
+                  <Slide className="carousel__inner-slideLarge px-5" index={3}>
+                    <Image
                       width={300}
                       height={300}
                       src="/crowdbotics.png"
                       alt="crowdbotics"
                     />
                   </Slide>
-                  <Slide className="carousel__inner-slideLarge px-2" index={4}>
-                    <img
+                  <Slide className="carousel__inner-slideLarge px-5" index={4}>
+                    <Image
                       width={300}
                       height={300}
                       src="/2marketing.png"
                       alt="2marketing"
                     />
                   </Slide>
-                  <Slide className="carousel__inner-slideLarge px-2" index={5}>
-                    <img
+                  <Slide className="carousel__inner-slideLarge px-5" index={5}>
+                    <Image
                       width={300}
                       height={300}
                       src="/gulf-academy.png"
                       alt="gulf academy"
                     />
                   </Slide>
-                  <Slide className="carousel__inner-slideLarge px-2" index={6}>
-                    <img
+                  <Slide className="carousel__inner-slideLarge px-5" index={6}>
+                    <Image
                       width={300}
                       height={300}
                       src="/happy-credit.webp"

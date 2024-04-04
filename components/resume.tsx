0e767a8fb62dd8654f7d2e41209eb01cb4521292
Wasif Jameel <wasif.jameel1@gmail.com>
2024-04-04 10:33:24 +0500

    experience resorting

--- components/resume.tsx
+++ components/resume.tsx
@@ -1,5 +1,81 @@
 import Image from 'next/image';
 
+const experiences = [
+  {
+    title: 'Lead Full Stack Engineer',
+    company: 'BTLR',
+    date: 'Sept 2023 - present',
+    achivements: [
+      'Revamped coding infrastructure to facilitate the transition of the existing system into a SaaS model, leading to an increase in user interest.',
+      'Formulated and executed an inclusive API plan, allowing external developers access to public APIs for building new applications, resulting in a 50% surge in third-party app connections.',
+      "Automated the organization's onboarding process, reducing the time it takes to become fully operational from 6 months to just 1 week, leading to an 80% decrease in onboarding costs.",
+    ],
+    skills:
+      'React Native, React, Node.js, Typescript, Python, MongoDB, PostgreSQL, TailwindCSS, AWS (EC2, Lambda functions, elastic beanstalk, CI/CD, S3), Chai, Unit Testing, Jira, Github, Github Copilot, Neo4j, Docker',
+  },
+  {
+    title: 'Senior Full Stack Developer',
+    company: 'Intermedia Marketing Group',
+    date: 'Feb 2018 - Nov 2022',
+    achivements: [
+      'Developed and maintained web solutions that contributed to scaling the business to a million dollars in revenue from a hundred thousand, focusing on enhancing user experience and reliability.',
+      'Implemented advanced tracking methods for user behavior, affiliate marketing, retargeting, Google Analytics, and pixel-based tracking, resulting in a 40% increase in website traffic and a 15% improvement in conversion rates.',
+      'Created visually appealing and dynamic web applications that adhere to industry standards, leading to a 20% increase in customer engagement and satisfaction.',
+      'Implemented a system to automate software testing procedures, resulting in a 30% increase in productivity and a 50% reduction in time spent on manual testing.',
+    ],
+    skills:
+      'React.js, Node.js, Gatsby, Next.js, TypeScript, JavaScript, TailwindCSS, RedTrack, Google Services, Facebook Pixel',
+  },
+  {
+    title: 'Senior Full Stack Developer',
+    company: 'Crowdbotics',
+    date: 'Jul 2020 - Jan 2021',
+    achivements: [
+      'Streamlined deployment process by creating automated build scripts for TestFlights and Android Builds, reducing review time by 50%.',
+      'Facilitated cross-functional collaboration with business, design, backend, and management teams to maintain project flow, resulting in on-time delivery of both mobile applications to the App Store / Play Store.',
+      'Utilized DevOps practices to increase stability and ensure seamless version control for both mobile applications, resulting in a 75% decrease in deployment errors.',
+      'Collaborated with different teams including business, design, backend, and management to ensure the smooth flow of the project.',
+    ],
+    skills:
+      'React-Native, Node.js, React.js, Socket.io, Python, PubNub, Google Cloud Messaging, OAuth, Facebook SDK',
+  },
+  {
+    title: 'Software Engineer',
+    company: 'CareCloud',
+    date: 'Jun 2018 - Nov 2018',
+    achivements: [
+      'Coordinated with senior leadership to develop a comprehensive roadmap for the Telemedicine application project, resulting in a successful launch.',
+      'Oversaw and facilitated over 100 pull requests and code reviews to ensure high-quality code standards were met by all team members, leading to a decrease in bugs by 15%.',
+      'Served as the primary liaison between cross-functional teams, ensuring seamless communication and collaboration throughout the development process of the Telemedicine application, resulting in an on-time delivery of the project.',
+    ],
+    skills:
+      'Angular, Node.js, React.js, TypeScript, MySQL, CI/CD, Socket.io, Cron Jobs.',
+  },
+  {
+    title: 'Jr. Developer',
+    company: 'Bitsol Technologies',
+    date: 'Jul 2015 - Sep 2015',
+    achivements: [
+      'Developed and implemented a suite of small utility software solutions to streamline internal processes, resulting in a 20% increase in efficiency.',
+      'Acquired proficiency in new technologies such as Python, JavaScript, and SQL to enhance programming skills and contribute to innovative projects.',
+      "Collaborated with cross-functional teams to identify logical tasks within the company's workflow, successfully resolving complex challenges and improving overall productivity.",
+    ],
+    skills: 'Angular, Node.js, React.js, TypeScript, MySQL, Python, MongoDB',
+  },
+  {
+    title: 'Self Employed Software Engineer',
+    Copmany: 'Freelance',
+    date: 'Jan 2016 - Present',
+    achivements: [
+      'Spearheaded project planning and technology decision-making for over 70 successful deployments, assisting 20 startups in entering the market and achieving success.',
+      'Implemented agile methodologies and utilized React, Nodejs, Next.js, TypeScript, AWS, Docker, and CI/CD pipelines to streamline project deadlines and increase efficiency by 30%.',
+      'Leveraged diverse industry experience to improve communication skills and foster teamwork within cross-functional teams, resulting in a 15% increase in project collaboration.',
+    ],
+    skills:
+      'Angular, Node.js, React.js, React Native, Next.js TypeScript, MySQL, Python, MongoDB, Docker, AWS, Heroku, Vercel',
+  },
+];
+
 const Resume = () => {
   return (
     <section className="Section5  bg-[#F8F9FE] py-12" id="resume">
@@ -82,241 +158,29 @@ const Resume = () => {
             </span>
             &nbsp; Experience
           </p>
-          <div className="cursor-pointer my-2 w-4/5">
-            <span className="flex flex-col items-start py-6 px-4 mx-auto bg-[#EDF1FD] text-[#737998] border border-gray-200 shadow-md  hover:scale-[1.1] ease-in duration-100">
-              <p className="text-[10px] text-start text-black opacity-[0.5]">
-                Sept 2023 - present
-              </p>
-              <p className="my-1 pt-2 text-lg text-start text-black  tracking-wide opacity-[0.8]">
-                Lead Full Stack Engineer
-              </p>
-              <ul className="font-light text-sm list-disc list-inside my-2">
-                <li>
-                  Revamped coding infrastructure to support the transition of
-                  the existing system into a SaaS model, leading to an increase
-                  in user interest.
-                </li>
-                <li>
-                  Developed and implemented a comprehensive API strategy,
-                  opening up public-facing APIs to external developers and
-                  enabling the creation of new applications on top of the
-                  existing system, resulting in a 50% increase in third-party
-                  app integrations.
-                </li>
-                <li>
-                  Automated the organization&apos;s onboarding process, reducing
-                  the time it takes to become fully operational from 6 months to
-                  just 1 week, leading to an 80% decrease in onboarding costs.
-                </li>
-              </ul>
-              <p className="text-sm my-3">
-                <b>Skills: </b> React Native, React, Node.js, Typescript,
-                Python, MongoDB, PostgreSQL, TailwindCSS, AWS (EC2, Lambda
-                functions, elastic beanstalk, CI/CD, S3), Chai, Unit Testing,
-                Jira, Github, Github Copilot, Neo4j, Docker
-              </p>
-              <p className="text-base text-start text-black opacity-[0.5]">
-                BTLR
-              </p>
-            </span>
-          </div>
-
-          <div className="cursor-pointer my-2 w-4/5">
-            <span className="flex flex-col items-start py-6 px-4 mx-auto bg-[#EDF1FD] text-[#737998] border border-gray-200 shadow-md  hover:scale-[1.1] ease-in duration-100">
-              <p className="text-[10px] text-start text-black opacity-[0.5]">
-                Jan 2023 - Sept 2023
-              </p>
-              <p className="my-1 pt-2 text-lg text-start text-black  tracking-wide opacity-[0.8]">
-                Senior Full Stack Engineer
-              </p>
-              <ul className="font-light text-sm list-disc list-inside my-2">
-                <li>
-                  Researched and selected the optimal technology stack for core
-                  application development, resulting in a 30% increase in
-                  overall system performance metrics.
-                </li>
-                <li>
-                  Established comprehensive coding standards and guidelines for
-                  peer programmers, resulting in a 15% decrease in code errors
-                  and a 20% increase in code efficiency.
-                </li>
-                <li>
-                  Built code pipelines to automate deployments on EC2, S3,
-                  Elastic Beanstalks, and Lambda functions.
-                </li>
-                <li>
-                  Scaled the applications to handle millions of users by
-                  transitioning from macro services to microservices for better
-                  scalability.
-                </li>
-              </ul>
-              <p className="text-sm my-3">
-                <b>Skills: </b> React Native, React, Node.js, Typescript,
-                MongoDB, PostgreSQL, TailwindCSS, AWS (EC2, Lambda functions,
-                elastic beanstalk, CI/CD, Code commit, S3), Chai, Unit Testing,
-                Trello, Github, Github Copilot, OpenAI,
-              </p>
-              <p className="text-base text-start text-black opacity-[0.5]">
-                BTLR
-              </p>
-            </span>
-          </div>
-
-          <div className="cursor-pointer my-2 w-4/5">
-            <span className="flex flex-col items-start py-6 px-4 mx-auto bg-[#EDF1FD] text-[#737998] border border-gray-200 shadow-md  hover:scale-[1.1] ease-in duration-100">
-              <p className="text-[10px] text-start text-black opacity-[0.5]">
-                Feb 2018 - Nov 2022
-              </p>
-              <p className="my-1 pt-2 text-lg text-start text-black  tracking-wide opacity-[0.8]">
-                Senior Full Stack Developer
-              </p>
-              <ul className="font-light text-sm list-disc list-inside my-2">
-                <li>
-                  Developed and maintained web solutions that contributed to
-                  scaling the business to a million dollars in revenue from a
-                  hundred thousand, focusing on enhancing user experience and
-                  reliability.
-                </li>
-                <li>
-                  Implemented advanced tracking methods for user behavior,
-                  affiliate marketing, retargeting, Google Analytics, and
-                  pixel-based tracking, resulting in a 40% increase in website
-                  traffic and a 15% improvement in conversion rates.
-                </li>
-                <li>
-                  Created visually appealing and dynamic web applications that
-                  adhere to industry standards, leading to a 20% increase in
-                  customer engagement and satisfaction.
-                </li>
-                <li>
-                  Implemented a system to automate software testing procedures,
-                  resulting in a 30% increase in productivity and a 50%
-                  reduction in time spent on manual testing.
-                </li>
-              </ul>
-              <p className="text-sm my-3">
-                <b>Skills: </b> React.js, Node.js, Gatsby, Next.js, TypeScript,
-                JavaScript, TailwindCSS, RedTrack, Google Services, Facebook
-                Pixel
-              </p>
-
-              <p className="text-[12px] text-start text-black opacity-[0.5]">
-                Intermedia Marketing Group
-              </p>
-            </span>
-          </div>
-          <div className="cursor-pointer my-2 w-4/5">
-            <span className="flex flex-col items-start py-6 px-4 mx-auto bg-[#EDF1FD] text-[#737998] border border-gray-200 shadow-md  hover:scale-[1.1] ease-in duration-100">
-              <p className="text-[10px] text-start text-black opacity-[0.5]">
-                Jul 2020 - Jan 2021
-              </p>
-              <p className="my-1 pt-2 text-lg text-start text-black  tracking-wide opacity-[0.8]">
-                Senior Full Stack Developer
-              </p>
-              <ul className="font-light text-sm list-disc list-inside my-2">
-                <li>
-                  Streamlined deployment process by creating automated build
-                  scripts for TestFlights and Android Builds, reducing review
-                  time by 50%.
-                </li>
-                <li>
-                  Facilitated cross-functional collaboration with business,
-                  design, backend, and management teams to maintain project
-                  flow, resulting in on-time delivery of both mobile
-                  applications to the App Store / Play Store.
-                </li>
-                <li>
-                  Utilized DevOps practices to increase stability and ensure
-                  seamless version control for both mobile applications,
-                  resulting in a 75% decrease in deployment errors.
-                </li>
-                <li>
-                  Collaborated with different teams including business, design,
-                  backend, and management to ensure the smooth flow of the
-                  project.
-                </li>
-              </ul>
-              <p className="text-sm my-3">
-                <b>Skills: </b> React-Native, Node.js, React.js, Socket.io,
-                Python, PubNub, Google Cloud Messaging, OAuth, Facebook SDK
-              </p>
-              <p className="text-[12px] text-start text-black opacity-[0.5]">
-                Crowdbotics
-              </p>
-            </span>
-          </div>
-
-          <div className="cursor-pointer my-2 w-4/5">
-            <span className="flex flex-col items-start py-6 px-4 mx-auto bg-[#EDF1FD] text-[#737998] border border-gray-200 shadow-md  hover:scale-[1.1] ease-in duration-100">
-              <p className="text-[10px] text-start text-black opacity-[0.5]">
-                Jun 2018 - Nov 2018
-              </p>
-              <p className="my-1 pt-2 text-lg text-start text-black  tracking-wide opacity-[0.8]">
-                Software Engineer
-              </p>
-              <ul className="font-light text-sm list-disc list-inside my-2">
-                <li>
-                  Coordinated with senior leadership to develop a comprehensive
-                  roadmap for the Telemedicine application project, resulting in
-                  a successful launch.
-                </li>
-                <li>
-                  Oversaw and facilitated over 100 pull requests and code
-                  reviews to ensure high-quality code standards were met by all
-                  team members, leading to a decrease in bugs by 15%.
-                </li>
-                <li>
-                  Served as the primary liaison between cross-functional teams,
-                  ensuring seamless communication and collaboration throughout
-                  the development process of the Telemedicine application,
-                  resulting in an on-time delivery of the project.
-                </li>
-              </ul>
-              <p className="text-sm my-3">
-                <b>Skills: </b> Angular, Node.js, React.js, TypeScript, MySQL,
-                CI/CD, Socket.io, Cron Jobs.
-              </p>
-              <p className="text-[12px] text-start text-black opacity-[0.5]">
-                CareCloud
-              </p>
-            </span>
-          </div>
-
-          <div className="cursor-pointer my-2 w-4/5">
-            <span className="flex flex-col items-start py-6 px-4 mx-auto bg-[#EDF1FD] text-[#737998] border border-gray-200 shadow-md  hover:scale-[1.1] ease-in duration-100">
-              <p className="text-[10px] text-start text-black opacity-[0.5]">
-                Jul 2015 - Sep 2015
-              </p>
-              <p className="my-1 pt-2 text-lg text-start text-black  tracking-wide opacity-[0.8]">
-                Jr. Developer
-              </p>
-              <ul className="font-light text-sm list-disc list-inside my-2">
-                <li>
-                  Developed and implemented a suite of small utility software
-                  solutions to streamline internal processes, resulting in a 20%
-                  increase in efficiency.
-                </li>
-                <li>
-                  Acquired proficiency in new technologies such as Python,
-                  JavaScript, and SQL to enhance programming skills and
-                  contribute to innovative projects.
-                </li>
-                <li>
-                  Collaborated with cross-functional teams to identify logical
-                  tasks within the company&apos;s workflow, successfully
-                  resolving complex challenges and improving overall
-                  productivity.
-                </li>
-              </ul>
-              <p className="text-sm my-3">
-                <b>Skills: </b> Angular, Node.js, React.js, TypeScript, MySQL,
-                Python, MongoDB
-              </p>
-              <p className="text-[12px] text-start text-black opacity-[0.5]">
-                Bitsol Technologies
-              </p>
-            </span>
-          </div>
+          {experiences.map(({ achivements, company, date, skills, title }) => (
+            <div className="cursor-pointer my-2 w-4/5">
+              <span className="flex flex-col items-start py-6 px-4 mx-auto bg-[#EDF1FD] text-[#737998] border border-gray-200 shadow-md  hover:scale-[1.1] ease-in duration-100">
+                <p className="text-[10px] text-start text-black opacity-[0.5]">
+                  {date}
+                </p>
+                <p className="my-1 pt-2 text-lg text-start text-black  tracking-wide opacity-[0.8]">
+                  {title}
+                </p>
+                <ul className="font-light text-sm list-disc list-inside my-2">
+                  {achivements.map((achivement) => (
+                    <li>{achivement}</li>
+                  ))}
+                </ul>
+                <p className="text-sm my-3">
+                  <b>Skills: </b> {skills}
+                </p>
+                <p className="text-base text-start text-black opacity-[0.5]">
+                  {company}
+                </p>
+              </span>
+            </div>
+          ))}
         </div>
       </div>
     </section>

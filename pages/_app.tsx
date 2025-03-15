a1f7a1712b6b85d97769ce40cfc95d93eba3ae57
Wasif Jameel <wasif.jameel1@gmail.com>
2025-03-15 19:29:37 +0500

    integration weglot

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import Script from 'next/script';
+import Head from 'next/head';
 
 import Layout from '../components/layout';
 
@@ -8,6 +9,15 @@ import '../styles/globals.css';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Head>
+        <link
+          rel="alternate"
+          hrefLang="en"
+          href="https://www.wasifjameel.com"
+        />
+        <link rel="alternate" hrefLang="ur" href="https://ur.wasifjameel.com" />
+      </Head>
+
       <Script
         type="text/javascript"
         src="https://www.googletagmanager.com/gtag/js?id=G-7EYEJD1P4P"
@@ -20,6 +30,19 @@ function MyApp({ Component, pageProps }: AppProps) {
           gtag('config', 'G-7EYEJD1P4P');
         `}
       </Script>
+
+      <Script
+        src="https://cdn.weglot.com/weglot.min.js"
+        strategy="beforeInteractive"
+      />
+      <Script id="weglot-init" strategy="afterInteractive">
+        {`
+          Weglot.initialize({
+            api_key: 'wg_d41c91e00c0096e0d5329d30f3ba1b5d7'
+          });
+        `}
+      </Script>
+
       <Component {...pageProps} />
     </Layout>
   );
